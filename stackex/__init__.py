09475854fa108e306df85f487b671c3be4e88aeb
Sean <boobookish@gmail.com>
2018-12-25 12:17:27 +0400

    Add interaction with config file

--- stackex/__init__.py
+++ stackex/__init__.py
@@ -1,11 +1,31 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+import configparser
+import os
 
-#import requests
+#Work with config.ini
+config_file = "config.ini"
+
+exist = os.path.isfile(config_file)
+config = configparser.ConfigParser()
+print(1)
+
+if not exist:
+    print(2)
+    print("Config file is not found")
+    config["DEFAULT"] = {"SECRET_KEY": "we_dont_have_secrets", 
+    "SQLALCHEMY_DATABASE_URI": 'sqlite:///site.db'}
+    with open(config_file ,"w") as configfile:
+        config.write(configfile)
+    print("Cofig file was created")
+    print("Please change secret key")
+
+config.read(config_file)
+default_config = config["DEFAULT"]
 
 app = Flask(__name__)
-app.config['SECRET_KEY']='yek_terces'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
+app.config['SECRET_KEY']=default_config['SECRET_KEY']
+app.config['SQLALCHEMY_DATABASE_URI'] = default_config['SQLALCHEMY_DATABASE_URI']
 db = SQLAlchemy(app)
 
 from stackex import routes
