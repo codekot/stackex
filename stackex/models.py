66d19a6d376bd968ae3ecec84fa59abe994637c6
sean <boobookish@gmail.com>
2019-01-18 12:28:45 +0400

    Add datetime type to models

--- stackex/models.py
+++ stackex/models.py
@@ -4,25 +4,23 @@ from datetime import datetime
 
 class User_request(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    date = db.Column(db.DateTime, nullable=False, default=datetime.now)
+    date = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
     req_name = db.Column(db.String(), unique=True, nullable=False)
-    # req_results = db.relationship('RequestResult',
-    # backref="on_request", lazy=True)
+    req_results = db.relationship('RequestResult',
+                                  backref="on_request", lazy=True)
 
     def __repr__(self):
-        return f"User request on subject '{self.req_name}' posted on \
-                {self.date}"
+        return f"User_request('{self.req_name}', {self.date})"
 
 
 class Request_result(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(), nullable=False)
-    date = db.Column(db.String(), nullable=False, default="some time")
-    # date = db.Column(db.DateTime, nullable=False, default=datetime.now)
+    last_activity_date = db.Column(db.DateTime, default=None)
     link = db.Column(db.Text())
     request_id = db.Column(db.Integer,
                            db.ForeignKey('user_request.id'),
                            nullable=False)
 
     def __repr__(self):
-        return f"RequestResult '{self.title}', '{self.date_posted}'"
+        return f"Request_result('{self.title}', '{self.date}')"
