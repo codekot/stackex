d7efc1680d2fe0182617f2957a402110fc614728
sean <boobookish@gmail.com>
2019-01-18 12:32:15 +0400

    Add if name==main to funcs

--- stackex/funcs.py
+++ stackex/funcs.py
@@ -1,12 +1,14 @@
 import json
 
-data_file = 'search.json'
-data = None
+if __name__ == '__main__':
+    data_file = 'search.json'
+    data = None
 
-with open(data_file, "r") as json_file:
-    data = json.load(json_file)
+    with open(data_file, "r") as json_file:
+        data = json.load(json_file)
 
-items = data['items']
-for item in items:
-    print(item['title'])
-    print(item["last_activity_date"])
+    items = data['items']
+    for item in items:
+        print(item['title'])
+        print(item["last_activity_date"])
+        print(item["link"])
