51d6bafbf0258d315591e31694c5c8dacb5c4fc9
sean <boobookish@gmail.com>
2019-02-06 17:44:04 +0400

    Add formatting to pagination

--- stackex/routes.py
+++ stackex/routes.py
@@ -30,8 +30,9 @@ def search_results(req):
     page = request.args.get('page', 1, type=int)
     #new_results = results_2
     find_request = User_request.query.filter_by(req_name=r).first().id
-    new_results = Request_result.query.filter_by(request_id=find_request).\
-        paginate(page=page, per_page=10)
+    new_results = Request_result.query.filter_by(request_id=find_request)\
+        .order_by(Request_result.last_activity_date.desc())\
+        .paginate(page=page, per_page=10)
     # new_results = new_results[:25]
     return render_template('search_results.html',
                            results=new_results, request=r)
