b6900bf8f1c9d7d006409cef8a1a7d7c638330a0
sean <boobookish@gmail.com>
2019-08-07 18:04:22 +0400

    Delete code for db population

--- stackex/funcs.py
+++ stackex/funcs.py
@@ -5,35 +5,6 @@ import requests
 from stackex import db
 
 
-def construct_list():
-    data_file = 'search.json'
-    data = None
-
-    with open(data_file, "r") as json_file:
-        data = json.load(json_file)
-
-    items = data['items']
-    result = [[item['question_id'],
-               item['title'],
-               datetime.utcfromtimestamp(item["last_activity_date"]),
-               item["link"]] for item in items]
-    return result
-
-
-def populate(db):
-    u = User_request(id=1, date=datetime.utcnow(), req_name="java")
-    db.session.add(u)
-    data = construct_list()
-    for datum in data:
-        rr = Request_result(id=datum[0],
-                            title=datum[1],
-                            last_activity_date=datum[2],
-                            link=datum[3],
-                            request_id=1)
-        db.session.add(rr)
-    db.session.commit()
-    print("Database is populated")
-
 def stack_request(req):
     #global db
     url = "https://api.stackexchange.com/2.2/search"
@@ -62,7 +33,6 @@ def stack_request(req):
         db.session.add(rr)
     db.session.commit()
     
-
 
 if __name__ == '__main__':
     pass
